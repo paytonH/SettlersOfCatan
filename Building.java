13e586612bd490c6b8c9ad4a04ce15bd7e6abf95
dreydyl <39103181+dreydyl@users.noreply.github.com>
2018-05-08 17:57:36 -0700

    Sample Building Class

---
+++ Building.java
@@ -0,0 +1,25 @@
+
+public class Building {
+
+  private int level;
+  private Player owner;
+  
+  public Building() {
+    level = 0;
+  }
+  
+  public void upgrade() {
+    if(level >= 2) {
+      System.out.println("Cannot do");
+      return;
+    }
+    level++;
+  }
+  
+  public boolean isASettlement() { return level==1; }
+  public boolean isACity() { return level==2; }
+  
+  public void setOwner(Player owner) { this.owner = owner; }
+  public Player getOwner() { return owner; }
+  
+}
